0be4788c4674a05a160de55e37a2c96d72b480fe
Fattigman <jacob.karlstrom@gmail.com>
2022-06-07 09:44:48 +0200

    Dict now fetches key value pair correctly

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
 
 @app.get("/users/me/items/")
 async def read_own_items(current_user: User = Depends(get_current_active_user)):
-    return [{"item_id": "Foo", "owner": current_user.username}]
+    return [{"item_id": "Foo", "owner": current_user['username']}]
 
 
 
