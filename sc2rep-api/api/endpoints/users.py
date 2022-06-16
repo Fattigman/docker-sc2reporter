10ec1d1f6adf301d7714769476c831516afd83b1
Fattigman <jacob.karlstrom@gmail.com>
2022-06-16 18:11:49 +0200

    created add user endpoint

--- sc2rep-api/api/endpoints/users.py
+++ sc2rep-api/api/endpoints/users.py
@@ -1,13 +1,14 @@
 
 from typing import List, Optional
-from fastapi import APIRouter, Depends, HTTPException, Query
-from starlette.responses import JSONResponse
+from fastapi import APIRouter, Depends
 
 from db import *
 from authentication import *
+from crud.users import create_user
+
+from models import User
 
 router = APIRouter()
-
 
 @router.get("/me/", response_model=User)
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
@@ -17,3 +18,8 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
 @router.get("/me/items/")
 async def read_own_items(current_user: User = Depends(get_current_active_user)):
     return [{"item_id": "Foo", "owner": current_user['username']}]
+
+@router.post("/add/{user: User}")
+async def post_user(user: User = Depends()):
+    create_user(user)
+    return user
