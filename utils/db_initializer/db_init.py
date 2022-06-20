87643ae930c666133ca7c30165b3106b48a5d034
Fattigman <jacob.karlstrom@gmail.com>
2022-06-20 12:02:55 +0200

    Better looking styling

--- utils/db_initializer/db_init.py
+++ utils/db_initializer/db_init.py
@@ -5,7 +5,7 @@ client = pymongo.MongoClient('localhost',27017)
 db_name = 'sarscov2_standalone'
 if db_name in client.list_database_names():
     print(f'{db_name} exists and will be deleted')
-    if input(f'Do you want to delete {db_name}') == 'y':
+    if input(f'Do you want to delete {db_name}: (y/n) ') == 'y':
         client.drop_database(f'{db_name}')
 else:
     print(f'{db_name} does not exist')
