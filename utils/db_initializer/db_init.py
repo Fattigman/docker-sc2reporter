bda5aa7094250dc81b1e8f7f9363cdcc68e199bf
Fattigman <jacob.karlstrom@gmail.com>
2022-06-20 11:58:50 +0200

    deletes the db if it already exists

---
+++ utils/db_initializer/db_init.py
@@ -0,0 +1,11 @@
+import pymongo
+# Check if the database exists
+# Delete the database if it exists
+client = pymongo.MongoClient('localhost',27017)
+db_name = 'sarscov2_standalone'
+if db_name in client.list_database_names():
+    print(f'{db_name} exists and will be deleted')
+    if input(f'Do you want to delete {db_name}') == 'y':
+        client.drop_database(f'{db_name}')
+else:
+    print(f'{db_name} does not exist')
