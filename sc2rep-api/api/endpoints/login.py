d018ee4c294c35df445d8cf164eb291c8525df06
Fattigman <jacob.karlstrom@gmail.com>
2022-09-12 15:36:40 +0200

    removed redundant dependencies

--- sc2rep-api/api/endpoints/login.py
+++ sc2rep-api/api/endpoints/login.py
@@ -5,20 +5,15 @@ from models import *
 
 from fastapi import Depends, FastAPI, HTTPException, status, Request, APIRouter
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
-
-from jose import JWTError, jwt
-from passlib.context import CryptContext
-from pydantic import BaseModel
-
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 router = APIRouter()
 
 @router.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+async def login_for_access_token(
+        form_data: OAuth2PasswordRequestForm = Depends()
+         ):
     user = await authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
