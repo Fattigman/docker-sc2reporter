2f69dd9c4020583a69396ba0fce8d68f0596d97f
Fattigman <jacob.karlstrom@gmail.com>
2022-09-16 09:56:15 +0200

    ractored test.py

--- sc2rep-api/test.py
+++ sc2rep-api/test.py
@@ -4,23 +4,25 @@ from api.main import app
 import json
 client = TestClient(app)
 
+def test_root():
+    response = client.get("/")
+    print('Testing "/" endpoint...')
+    assert response.status_code == 200
+    print('OK!')
+
 def test_login():
     payload = "grant_type=&username=Test&password=Test&client_id=&client_secret="
-    response = client.post('/login/token/', 
+    response = client.post('/login/token', 
         headers={'Content-Type': 'application/x-www-form-urlencoded', 'accept': 'application/json'},
         data=payload,
         )
     print('Testing /login/token...')
-    print(vars(response))
     assert response.status_code == 200
     print('OK!')
 
 
 def test_read_main():
-    response = client.get("/")
-    print('Testing "/" endpoint...')
-    assert response.status_code == 200
-    print('OK!')
+    test_root()
     test_login()
     
 
