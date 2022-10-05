f2e53a63e111e52e92686c694b31788d59908dcd
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2022-10-05 14:54:30 +0200

    Add Descriptions

--- frontend/src/pages/PangolinPage.tsx
+++ frontend/src/pages/PangolinPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { Card } from 'antd'
+import { Card, Descriptions } from 'antd'
 import { SamplesTable } from 'components/SamplesTable'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { getPangolin } from 'services/api'
 
 export const PangolinPage = ({ token }) => {
@@ -16,12 +16,27 @@ export const PangolinPage = ({ token }) => {
   }, [id])
 
   return (
-    <Card
-      title={`Pangolin ${id}`}
-      extra={
-        <a href={`https://cov-lineages.org/lineage.html?lineage=${id}`}>Lineage information</a>
-      }
-    >
+    <Card>
+      {pangolin && (
+        <Descriptions
+          title={`Pangolin ${id}`}
+          bordered
+          size="small"
+          style={{ margin: 20 }}
+          column={2}
+        >
+          <Descriptions.Item label="Pangolearn version">
+            {pangolin[0]?.pangolin?.pangolearn_version}
+          </Descriptions.Item>
+          <Descriptions.Item label="Nextclade">
+            <Link to={`/nextclade/${pangolin[0]?.nextclade}`}>{pangolin[0]?.nextclade}</Link>
+          </Descriptions.Item>
+          <Descriptions.Item label="Lineage information">
+            <a href={`https://cov-lineages.org/lineage.html?lineage=${id}`}>{id}</a>
+          </Descriptions.Item>
+        </Descriptions>
+      )}
+
       <SamplesTable samples={pangolin} />
     </Card>
   )
