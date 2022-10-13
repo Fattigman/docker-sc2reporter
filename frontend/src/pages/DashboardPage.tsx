7cea44f68ad2c0b20cd0dd509dd76f980cdfd19c
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2022-10-13 11:53:19 +0200

    Add mock data

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getDashboard } from 'services/api'
-import { Area } from '@ant-design/plots'
+import { DualAxes } from '@ant-design/plots'
 
 export const DashboardPage = ({ token }) => {
   const [data, setData] = useState<any>()
@@ -10,12 +10,151 @@ export const DashboardPage = ({ token }) => {
 
   console.log(data)
 
-  const config = {
+  const graph = [
+    {
+      date: '2021-03-01',
+      value: 0,
+      pangolin: 'B.1.177.21',
+      count: 100,
+    },
+    {
+      date: '2021-03-02',
+      value: 2,
+      pangolin: 'Undetermined',
+      count: 90,
+    },
+    {
+      date: '2021-03-03',
+      value: 3,
+      pangolin: 'Undetermined',
+      count: 80,
+    },
+    {
+      date: '2021-03-04',
+      value: 3,
+      pangolin: 'Undetermined',
+      count: 60,
+    },
+    {
+      date: '2021-03-05',
+      value: 2,
+      pangolin: 'Undetermined',
+      count: 50,
+    },
+    {
+      date: '2021-03-06',
+      value: 3,
+      pangolin: 'B.1.1.7',
+      count: 10,
+    },
+    {
+      date: '2021-03-07',
+      value: 2,
+      pangolin: 'B.1.1.7',
+      count: 20,
+    },
+    {
+      date: '2021-03-08',
+      value: 2,
+      pangolin: 'B.1.1.39',
+      count: 30,
+    },
+    {
+      date: '2021-03-09',
+      value: 2,
+      pangolin: 'B.1.177.80',
+      count: 40,
+    },
+    {
+      date: '2021-03-10',
+      value: 3,
+      pangolin: 'B.1.1.7',
+      count: 100,
+    },
+    {
+      date: '2021-03-10',
+      value: 5,
+      pangolin: 'B.1.1.7',
+      count: 70,
+    },
+    {
+      date: '2021-03-10',
+      value: 2,
+      pangolin: 'B.1.474',
+      count: 100,
+    },
+    {
+      date: '2021-03-09',
+      value: 7,
+      pangolin: 'B.1.160',
+      count: 100,
+    },
+    {
+      date: '2021-03-09',
+      value: 4,
+      pangolin: 'B.1.1.7',
+      count: 100,
+    },
+    {
+      date: '2021-03-07',
+      value: 2,
+      pangolin: 'B.1.177.86',
+      count: 100,
+    },
+    {
+      date: '2021-03-08',
+      value: 2,
+      pangolin: 'B.1',
+      count: 100,
+    },
+    {
+      date: '2021-03-08',
+      value: 2,
+      pangolin: 'B.1.160',
+      count: 100,
+    },
+    {
+      date: '2021-03-11',
+      value: 4,
+      pangolin: 'B.1.1.7',
+      count: 100,
+    },
+    {
+      date: '2021-03-10',
+      value: 2,
+      pangolin: 'B.1.160',
+      count: 100,
+    },
+  ]
+
+  /* const config = {
     data,
     xField: 'date',
     yField: 'value',
     seriesField: 'pangolin',
+  } */
+
+  const config = {
+    data: [graph, graph],
+    xField: 'date',
+    yField: ['count', 'value'],
+    geometryOptions: [
+      {
+        geometry: 'line',
+        seriesField: 'pangolin',
+        lineStyle: {
+          lineWidth: 3,
+          lineDash: [5, 5],
+        },
+        smooth: true,
+      },
+      {
+        geometry: 'line',
+        seriesField: 'name',
+        point: {},
+      },
+    ],
   }
 
-  return <>{data ? <Area {...config} /> : <h1>DashboardPage</h1>}</>
+  return <>{data ? <DualAxes {...config} /> : <h1>DashboardPage</h1>}</>
 }
