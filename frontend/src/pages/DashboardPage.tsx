8236e848a38dd37a15a22caa6900171549f25bc6
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2022-10-25 13:21:36 +0200

    Add description

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,160 +1,56 @@
 import React, { useEffect, useState } from 'react'
 import { getDashboard } from 'services/api'
-import { DualAxes } from '@ant-design/plots'
+import { Area } from '@ant-design/plots'
+import { Card, Descriptions, PageHeader } from 'antd'
 
 export const DashboardPage = ({ token }) => {
   const [data, setData] = useState<any>()
+  const [generalStats, setGeneralStats] = useState<any>()
   useEffect(() => {
-    getDashboard(token).then((response) => setData(response.graph))
+    getDashboard(token).then((response) => {
+      setData(response.dashboard_data.sort(customSort))
+      setGeneralStats(response.general_stats)
+    })
   }, [])
 
-  console.log(data)
-
-  const graph = [
-    {
-      date: '2021-03-01',
-      value: 0,
-      pangolin: 'B.1.177.21',
-      count: 100,
-    },
-    {
-      date: '2021-03-02',
-      value: 2,
-      pangolin: 'Undetermined',
-      count: 90,
-    },
-    {
-      date: '2021-03-03',
-      value: 3,
-      pangolin: 'Undetermined',
-      count: 80,
-    },
-    {
-      date: '2021-03-04',
-      value: 3,
-      pangolin: 'Undetermined',
-      count: 60,
-    },
-    {
-      date: '2021-03-05',
-      value: 2,
-      pangolin: 'Undetermined',
-      count: 50,
-    },
-    {
-      date: '2021-03-06',
-      value: 3,
-      pangolin: 'B.1.1.7',
-      count: 10,
-    },
-    {
-      date: '2021-03-07',
-      value: 2,
-      pangolin: 'B.1.1.7',
-      count: 20,
-    },
-    {
-      date: '2021-03-08',
-      value: 2,
-      pangolin: 'B.1.1.39',
-      count: 30,
-    },
-    {
-      date: '2021-03-09',
-      value: 2,
-      pangolin: 'B.1.177.80',
-      count: 40,
-    },
-    {
-      date: '2021-03-10',
-      value: 3,
-      pangolin: 'B.1.1.7',
-      count: 100,
-    },
-    {
-      date: '2021-03-10',
-      value: 5,
-      pangolin: 'B.1.1.7',
-      count: 70,
-    },
-    {
-      date: '2021-03-10',
-      value: 2,
-      pangolin: 'B.1.474',
-      count: 100,
-    },
-    {
-      date: '2021-03-09',
-      value: 7,
-      pangolin: 'B.1.160',
-      count: 100,
-    },
-    {
-      date: '2021-03-09',
-      value: 4,
-      pangolin: 'B.1.1.7',
-      count: 100,
-    },
-    {
-      date: '2021-03-07',
-      value: 2,
-      pangolin: 'B.1.177.86',
-      count: 100,
-    },
-    {
-      date: '2021-03-08',
-      value: 2,
-      pangolin: 'B.1',
-      count: 100,
-    },
-    {
-      date: '2021-03-08',
-      value: 2,
-      pangolin: 'B.1.160',
-      count: 100,
-    },
-    {
-      date: '2021-03-11',
-      value: 4,
-      pangolin: 'B.1.1.7',
-      count: 100,
-    },
-    {
-      date: '2021-03-10',
-      value: 2,
-      pangolin: 'B.1.160',
-      count: 100,
-    },
-  ]
-
-  /* const config = {
-    data,
-    xField: 'date',
-    yField: 'value',
-    seriesField: 'pangolin',
-  } */
-
-  const config = {
-    data: [graph, graph],
-    xField: 'date',
-    yField: ['count', 'value'],
-    geometryOptions: [
-      {
-        geometry: 'line',
-        seriesField: 'pangolin',
-        lineStyle: {
-          lineWidth: 3,
-          lineDash: [5, 5],
-        },
-        smooth: true,
-      },
-      {
-        geometry: 'line',
-        seriesField: 'name',
-        point: {},
-      },
-    ],
+  const customSort = (a, b) => {
+    const dateA = new Date(a.date)
+    const dateB = new Date(b.date)
+    if (dateA > dateB) return 1
+    else if (dateA < dateB) return -1
+    return 0
   }
 
-  return <>{data ? <DualAxes {...config} /> : <h1>DashboardPage</h1>}</>
+  const config = {
+    data,
+    xField: 'date',
+    yField: 'pango_count',
+    seriesField: 'pangolin',
+  }
+
+  return data ? (
+    <Card>
+      <PageHeader onBack={() => history.back()} title={'Dashboard'}>
+        <>
+          <Descriptions bordered size="small">
+            <Descriptions.Item label="Passed qc samples">
+              {generalStats.passed_qc_samples}
+            </Descriptions.Item>
+            <Descriptions.Item label="Unique mutations">
+              {generalStats.unique_mutations}
+            </Descriptions.Item>
+            <Descriptions.Item label="Unique pangos">
+              {generalStats.unique_pangos}
+            </Descriptions.Item>
+          </Descriptions>
+          <br />
+          <Card>
+            <Area {...config} />
+          </Card>
+        </>
+      </PageHeader>
+    </Card>
+  ) : (
+    <h2>DashboardPage</h2>
+  )
 }
