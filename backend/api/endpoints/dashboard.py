c34ce6675d7532c48a46601e6b2b0400f4b4c87d
Fattigman <jacob.karlstrom@gmail.com>
2022-11-09 14:54:00 +0100

    Reworked the fetching of data to mongodb queries

--- backend/api/endpoints/dashboard.py
+++ backend/api/endpoints/dashboard.py
@@ -10,52 +10,12 @@ import time
 import datetime
 
 router = APIRouter()
-
-
-def create_general_stats(samples: list, graph_list:list, mutations: set):
-    general_stats = dict()
-    passed_qc = [s for s in samples if s['qc']['qc_pass'] == 'TRUE']
-    unique_pangos = set([p['pangolin'] for p in graph_list if p['pangolin'] != 'Undetermined'])
-    general_stats['passed_qc_samples'], general_stats['unique_pangos'] = len(passed_qc), len(unique_pangos)
-    general_stats['unique_mutations'] = len(mutations)
-    return general_stats
-
-def create_graph_obj(samples: list):
-    graph_list = list()
-    mutations = set()
-    for sample in samples:
-        if 'collection_date' and 'variants' in sample.keys():
-            sample_time = (time.strftime('%Y-%m-%w', time.localtime(sample['collection_date']['$date']/1000))) # convert epoch to datetime
-            sample_pango = sample['pangolin']['type']
-            [mutations.add(x['id']) for x in sample['variants']]
-            if sample_pango == 'None': sample_pango = 'Undetermined'
-            if len(graph_list) == 0: graph_list.append({'date':sample_time, 'pango_count': 1, 'pangolin': sample_pango})
-            else:
-                for counter, graph_obj in enumerate(graph_list):
-                    if graph_obj['date'] == sample_time and graph_obj['pangolin'] == sample_pango: 
-                        graph_obj['pango_count'] += 1
-                        counter -= 1
-                        break
-                    elif counter+1 == len(graph_list):
-                        graph_list.append({'date':sample_time, 'pango_count': 1, 'pangolin': sample_pango})
-        else: print(f'{sample["sample_id"]} doesnt have collection_date or variant data')
-    graph_list = sorted(graph_list,key=lambda d: d['date'])
-    return graph_list, mutations
-
-
-@router.post("/",
- response_model=DashboardGraph
- )
-async def get_dashboard(
-    current_user: User = Depends(get_current_active_user),
-    selection_criterions:list[Literal['Reinfektion', 'Vaccingenombrott']] | None = Query(default=None),
-    ):
-    if not selection_criterions: query = None
-    else: query = {'selection_criterion': {'$in': selection_criterions}}
-    samples = await get_samples(query=query)
-    
-    graph_list, mutations = create_graph_obj(samples)
-
-    general_stats = create_general_stats(samples, graph_list, mutations)
-
-    return {'dashboard_data':graph_list, 'general_stats': general_stats}
+@router.post("/", response_model=DashboardGraph)
+async def get_dashboard_data(
+    selection_criterion: list = Query([]),
+    current_user: User = Depends(get_current_active_user)
+):
+    graph_list = await group_by_samples(selection_criterion)
+    selection_criterions = await get_selection_criterions()
+    general_stats = await get_general_stats()
+    return {'general_stats':general_stats,'graph_list':graph_list, 'selection_criterions':selection_criterions}
--- backend/crud/samples.py
+++ backend/crud/samples.py
@@ -32,7 +32,146 @@ async def get_nextclade_samples(nextclade:str):
     curr =  db.sample.find({"nextclade": nextclade})
     docs = [parse_json(x) for x in await curr.to_list(None)]
     return  docs
+def flatten(l):
+    return [item for sublist in l for item in sublist]
 
+async def get_selection_criterions():
+    curr = db.sample.aggregate([
+        {
+            '$group': {
+                '_id': {
+                    'selection_criterion': '$selection_criterion',
+                }
+            }
+        }, {
+            '$project': {
+                '_id': 0,
+                'selection_criterion': '$_id.selection_criterion',
+            }
+        }
+    ])
+    docs = [parse_json(x) for x in await curr.to_list(None)]
+    docs =   flatten([[doc[key] for key in doc if doc[key] != None ] for doc in docs])
+    return  docs
+
+
+
+async def group_by_samples(selection_criterion: list = []) :
+    if selection_criterion == []:
+        selection_criterion = await get_selection_criterions()
+    curr = db.sample.aggregate([
+        {
+            # Group by pangolin type, selection criterion and selection_criterion
+            '$group': {
+                '_id': {
+                    'pangolin': '$pangolin.type',
+                    'date': '$collection_date',
+                    'selection_criterion': '$selection_criterion',
+                },
+                'pango_count': {
+                    '$sum': 1
+                },
+                
+            },
+        },{
+            # flatten the _id field
+            '$project': {
+                'pangolin': '$_id.pangolin',
+                'date': '$_id.date',
+                'pango_count': 1,
+                'selection_criterion': '$_id.selection_criterion',
+                '_id': 0
+            }
+        },{
+            # convert date to datetime
+            '$addFields': {
+                'date': {
+                    '$dateToString': {
+                        'format': '%Y-%m-%d',
+                        'date': '$date'
+                    }
+                }
+            }
+        },{
+            # filter out where "date" is "None"
+            '$match': {
+                'pangolin': {
+                    '$ne': 'None'
+                }
+            }
+        },{
+        #filter by selection_criterion
+            '$match': {
+                'selection_criterion': {
+                    '$in': selection_criterion
+                    }
+             }
+        },{
+            # Remove selection_criterion and group again
+            '$group': {
+                '_id': {
+                    'pangolin': '$pangolin',
+                    'date': '$date',
+                },
+                'pango_count': {
+                    '$sum': '$pango_count'
+                },
+            }
+        },{
+            # flatten the _id field
+            '$project': {
+                'pangolin': '$_id.pangolin',
+                'date': '$_id.date',
+                'pango_count': 1,
+                '_id': 0
+            }
+        },{
+            # sort by date
+            '$sort': {
+                'date': 1
+            }
+        }
+
+    ])
+    docs = [parse_json(x) for x in await curr.to_list(None)]
+    return  docs
+
+async def get_general_stats():
+    curr = db.sample.aggregate([
+        {
+            '$group': {
+                '_id': {
+                    'sample_name': '$qc.qc_pass',
+                },
+                'passed_qc_samples': {
+                    '$sum': 1
+                },
+            }
+        },{ 
+            #filter out where sample_name is "FALSE"
+            '$match': {
+                '_id.sample_name': {
+                    '$ne': "FALSE"
+                }
+            }
+        },{ #Flatten the _id field
+            '$project': {
+                '_id': 0,
+                'passed_qc_samples': 1,
+            }
+        }
+        ])
+    docs = await curr.to_list(None)
+    qc_samples = docs[0]['passed_qc_samples']
+
+    curr = db.sample.distinct('pangolin.type')
+    unique_pangos = await curr
+    unique_pangos = len([x for x in unique_pangos if x != 'None'])
+
+    curr = db.variant.distinct('_id')
+    unique_variants = await curr
+    unique_variants = len([x for x in unique_variants])
+    return  {'passed_qc_samples':qc_samples,'unique_pangos':unique_pangos, 'unique_mutations':unique_variants}
 # Delete samples
 
 async def delete_single_sample(sample_id : str):
