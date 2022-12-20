6e37057d6976dc9483fde3f5c210611606303e08
Fattigman <jacob.karlstrom@gmail.com>
2022-12-20 09:31:28 +0100

    refactored models into separe .py files

---
+++ backend/models/__init__.py
@@ -0,0 +1,4 @@
+from .sample import Sample, GroupedSamples, Variant, Pangolin
+from .user import User, Basic_User
+from .graph import SimpleGraphElement, DashBoardGraphElement, GeneralStats, DashboardGraph
+from .login import Token, TokenData, UserInDB
---
+++ backend/models/user.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import Union, Literal
+
+"""
+Here we define the models for the users.
+In order not to leak the password to the frontend,
+we have two models, one for the user and one for the admin.
+"""
+class Basic_User(BaseModel):
+    username: str 
+    email: str
+    fullname: str
+    disabled: bool = False
+    scope: Literal['user','admin']
+
+class User(Basic_User):
+    password: str
