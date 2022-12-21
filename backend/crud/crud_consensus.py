aa81ca52677424eb43234167d67ee4e7fe37eebb
Fattigman <jacob.karlstrom@gmail.com>
2022-12-21 13:20:49 +0100

    Created a consensus obejct from CRUDBase

---
+++ backend/crud/crud_consensus.py
@@ -0,0 +1,8 @@
+from .crud_base import CRUDBase
+from models import Consensus
+
+class CRUDConsensus(CRUDBase):
+    def __init__(self):
+        super().__init__(Consensus, "consensus")
+
+consensus = CRUDConsensus()
