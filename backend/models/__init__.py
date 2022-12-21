10955345a4c71ce3d03ee4b8a558e4c57d238672
Fattigman <jacob.karlstrom@gmail.com>
2022-12-21 13:22:20 +0100

    Added Consensus to model exports

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -4,7 +4,7 @@ The models are used to define the structure of the data.
 They are also used to define the structure of the responses.
 Import them into endpoints and use them for responses and inputs.
 """
-from .sample import Sample, GroupedSamples, Variant, Pangolin
+from .sample import Sample, GroupedSamples, Variant, Pangolin, Consensus
 from .user import User, Basic_User
 from .graph import SimpleGraphElement, DashBoardGraphElement, GeneralStats, DashboardGraph
 from .login import Token, TokenData, UserInDB
