6014f17959610e64fb7ad364463fb145dd55ad26
Fattigman <jacob.karlstrom@gmail.com>
2023-01-17 12:07:03 +0100

    refactored variants crud functionality

--- backend/api/endpoints/variants.py
+++ backend/api/endpoints/variants.py
@@ -1,33 +1,74 @@
-from typing import Optional, Union
-from fastapi import APIRouter, Depends, HTTPException, Query
-from starlette.responses import JSONResponse
+from fastapi import APIRouter, Depends, Query, Body, HTTPException, status
+from crud import variants
+from models import User, Variant
+from authentication import get_current_active_user
+from typing import List
 
-from crud import get_multiple_variants, get_single_variant, get_variants
-from models import *
-from authentication import *
-
+"""
+This module contains all the endpoints for the Variant collection in the database.
+They use the functions from the crud module to interact with the database with the Variant model.
+"""
 router = APIRouter()
 
-
-@router.get("/")
-async def get_variant_info(
+@router.get("/", response_model=List[Variant]
+)
+async def get_Variant_endpoint(
     current_user: User = Depends(get_current_active_user)
-    ):
-    samples = await get_variants()
+):  
+    data = await variants.get()
+    return data
 
-    return samples
-
-@router.get("/{variant_id}")
-async def single_sample(
-    variant_id: str,
+@router.get("/{id}", response_model=List[Variant])
+async def get_single_Variant_endpoint(
+    id: str = Query(..., description="The sample id of the Variant sequence to get"),
     current_user: User = Depends(get_current_active_user)
-    ):
-    return await get_single_variant(variant_id)
+):  
+    data = await variants.get_single(id, "sample_id")
 
-@router.post("/multiple/")
-async def multiple_samples(
-    variant_ids:list[str] | None = Query(default=None),
+    if len(data) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Variant sequence can't be found in database",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return data
+
+@router.get("/multiple", response_model=List[Variant])
+async def get_multiple_Variant_endpoint(
+    ids: List[str] = Query(..., description="List of sample ids of the Variant sequences to get"),
     current_user: User = Depends(get_current_active_user)
-    ):
-    return await get_multiple_variants(variant_ids)
+):  
+    data = await variants.get_multiple(ids, "sample_id")
+    if len(data) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Variant sequences can't be found in database",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return data
 
+@router.post("/")
+async def create_Variant_endpoint(
+    obj_in: Variant = Body(..., description="The Variant object to create"),
+    current_user: User = Depends(get_current_active_user)
+):
+    data = await variants.create(obj_in)
+    if data:
+        return {f'{obj_in.sample_id}': 'created successfully'}
+    else:
+        return {f'{obj_in.sample_id}': 'failed to create'}
+
+@router.put("/{id}")
+async def update_Variant_endpoint(
+    id: str,
+    obj_in: Variant,
+    current_user: User = Depends(get_current_active_user)
+):
+    data = await variants.update(id, obj_in, "sample_id")
+    if not data:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Sample failed to update in the database",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return {f'{id}': 'failed to update'}
