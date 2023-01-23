3fa2dba77c136ae3676d57735ec1302a7db3aba3
Fattigman <jacob.karlstrom@gmail.com>
2023-01-23 23:16:00 +0100

    removed deprecated funtions

--- backend/crud/crud_misc.py
+++ backend/crud/crud_misc.py
@@ -7,8 +7,3 @@ async def get_depth(query: Optional[any] = None):
     docs = [parse_json(x) for x in await cursor.to_list(None)]
     return (docs)
 
-# Handling of the consensus collection
-async def get_consensus(query: Optional[any] = None):
-    cursor =  db.consensus.find(query)
-    docs = [parse_json(x) for x in await cursor.to_list(None)]
-    return (docs)
