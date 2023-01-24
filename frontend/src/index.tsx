c4b16875fb4ffee2c59fb3b6fa610707c867412a
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2023-01-24 17:01:18 +0100

    Change PUBLIC_URL place

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,10 +6,10 @@ import { App } from './App'
 import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-const prefix = 'v1'
+const { PUBLIC_URL } = process.env
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={prefix}>
+    <BrowserRouter basename={PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
