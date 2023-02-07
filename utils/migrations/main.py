99931cfa512589b5719b4b3ff4f1e2bd36eb4bce
Fattigman <jacob.karlstrom@gmail.com>
2023-02-07 11:23:54 +0100

    added migration scripts from v1 to v2

---
+++ utils/migrations/main.py
@@ -0,0 +1,57 @@
+# This will only be used to migrate from version 1.0.0 to 2.0.0
+# This will be removed in future versions
+
+# imports appropiate packages
+import pandas as pd
+import numpy as np
+import pymongo
+
+from passlib.context import CryptContext
+from pymongo.errors import DuplicateKeyError
+
+# creates the matrix
+def create_matrix():
+    db = pymongo.MongoClient('localhost',27017).sarscov2_standalone
+    samples = [x for x in db.sample.find()]
+    variants = [x for x in db.variant.find()]
+    depth = [x for x in db.depth.find()]
+
+    df = pd.DataFrame()
+    col_name = ''
+    col = []
+    index = []
+    for i, ele in enumerate(depth):
+        if i == 0:
+            col_name = ele['sample_id']
+        if ele['sample_id'] != col_name:
+            df[col_name] = col
+            col_name = ele['sample_id']
+            col = []
+            index = []
+        temp = [ele['A'], ele['T'], ele['C'], ele['G']]
+        col.extend([x/max(temp)  if max(temp) != 0 else np.nan for x in temp] )
+        index.extend([f'{ele["pos"]}_A', f'{ele["pos"]}_T', f'{ele["pos"]}_C', f'{ele["pos"]}_G'])
+        
+    df.index = index
+        
+    df = df.transpose().fillna(df.transpose().mean()).transpose()
+
+    dict_df = df.to_dict('list')
+    db['matrix'].insert_one(dict_df)
+
+# creates the superuser
+def create_superuser():
+    db = pymongo.MongoClient('localhost',27017).sarscov2_standalone
+    db.users.drop()
+    # create user collection
+    db.create_collection('users')
+    hashed_password = CryptContext(schemes=["bcrypt"], deprecated="auto").hash('admin')
+    db_user = 'admin'
+    collection = db.users
+
+    try:
+        collection.insert_one({"_id":db_user.username,"username": db_user.username, "password": hashed_password, "email": db_user.email, "fullname": db_user.fullname, "disabled":False, "scope":'admin'})
+        print("User created.")
+    except DuplicateKeyError:
+        print ('User already exists in the database')
+    return db_user
