6a6e01a68bc5b00682d68e63159b6d45d4624353
Fattigman <jacob.karlstrom@gmail.com>
2023-02-10 15:43:48 +0100

    Changed collection name from depth to significan_variants

--- backend/crud/crud_significant_variants.py
+++ backend/crud/crud_significant_variants.py
@@ -4,6 +4,6 @@ from models import SignificantVariant
 # Handling of the depth collection
 class CRUDSignificantVariants(CRUDBase[SignificantVariant, SignificantVariant]):
     def __init__(self):
-        super().__init__(SignificantVariant, "depth")
+        super().__init__(SignificantVariant, "significant_variants")
 
 significant_variants = CRUDSignificantVariants()
