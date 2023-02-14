dcc4ff30aa1b0a2b722a3d728281a3ca01f3b918
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2023-02-14 15:36:37 +0100

    Add REACT_APP_PREFIX_URL to docker

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const { REACT_APP_PREFIX_URL } = process.env
+
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={REACT_APP_PREFIX_URL}>
