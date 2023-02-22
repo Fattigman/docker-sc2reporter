dc46886c501917f3f2a2ac4ce5c45d931a71125c
Fattigman <jacob.karlstrom@gmail.com>
2023-02-22 09:41:28 +0100

    migrations now chunks the data when calculating matrix

--- utils/migrations/main.py
+++ utils/migrations/main.py
@@ -11,31 +11,24 @@ from pymongo.errors import DuplicateKeyError
 from pprint import pprint
 # creates the matrix
 def create_matrix(db_name:str='sarscov2_standalone'):
+
     db = pymongo.MongoClient('localhost',27017)[db_name]
-    samples = [x for x in db.sample.find()]
-    variants = [x for x in db.variant.find()]
-    depth = [x for x in db.depth.find()]
-
     df = pd.DataFrame()
-    col_name = ''
-    col = []
-    index = []
-    for i, ele in enumerate(tqdm(depth)):
-        if i == 0:
-            col_name = ele['sample_id']
-        if ele['sample_id'] != col_name:
-            df[col_name] = col
-            col_name = ele['sample_id']
-            col = []
-            index = []
-        temp = [ele['A'], ele['T'], ele['C'], ele['G']]
-        col.extend([x/max(temp)  if max(temp) != 0 else np.nan for x in temp] )
-        index.extend([f'{ele["pos"]}_A', f'{ele["pos"]}_T', f'{ele["pos"]}_C', f'{ele["pos"]}_G'])
-        
-    df.index = index
-        
-    df = df.transpose().fillna(df.transpose().mean()).transpose()
+    
+    # Get all sample names to iterate over
+    sample_names = [x['sample_id'] for x in db.sample.find()]
+    for name in tqdm(sample_names):
+        col = []
+        index = []
+        # Chunk the data to avoid memory issues
+        for i, ele in enumerate(db.depth.find({'sample_id':name})):
+            temp = [ele['A'], ele['T'], ele['C'], ele['G']]
+            col.extend([x/max(temp)  if max(temp) != 0 else np.nan for x in temp] )
+            index.extend([f'{ele["pos"]}_A', f'{ele["pos"]}_T', f'{ele["pos"]}_C', f'{ele["pos"]}_G'])
+        df[name] = col
 
+    df.index = index
+    df = df.transpose().fillna(df.transpose().mean()).transpose()
     dict_df = df.to_dict('list')
     print ('Inserting samples into matrix...')
     for i, ele in enumerate(tqdm(dict_df)):
