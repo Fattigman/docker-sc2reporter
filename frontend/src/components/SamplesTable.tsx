b896a56c854a333ce82e89b3c040672f924fe4e4
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2023-02-22 14:04:35 +0100

    Copy Phyllogeny data to clipboard

--- frontend/src/components/SamplesTable.tsx
+++ frontend/src/components/SamplesTable.tsx
@@ -1,23 +1,30 @@
 import React, { useState } from 'react'
-import { Button, notification, Popconfirm, Table, Tag } from 'antd'
+import { Button, Modal, notification, Popconfirm, Table, Tag } from 'antd'
 import { formatDate, sortDate } from '../helpers'
-import { CheckCircleTwoTone, DeleteTwoTone } from '@ant-design/icons'
+import { CheckCircleTwoTone } from '@ant-design/icons'
 import { Link, useLocation } from 'react-router-dom'
-import { deleteSample } from 'services/api'
+import { deleteSample, getPhyllogeny } from 'services/api'
+import styles from './SamplesTable.module.css'
 
 export const SamplesTable = ({ token, samples, refreshSamples, isAdmin }) => {
   const [selectedRowKeys, setSelectedRowKeys] = useState<any[]>([])
   const [samplesId, setSamplesId] = useState<string>('')
+  const [samplesId2, setSamplesId2] = useState<string>('')
+  const [copiedText, setCopiedText] = useState<string>('')
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
   const location = useLocation()
   const nextclade = 'nextclade'
   const pangolin = 'pangolin'
+  const group = 'samples'
   const hasSelected = selectedRowKeys.length > 0
 
   const rowSelection = {
     onChange: (selectedRowKeys) => {
       setSelectedRowKeys(selectedRowKeys)
       const selectedSamples = selectedRowKeys.map((item) => `&sample_ids=${item}`).join('')
+      const selectedSamples2 = selectedRowKeys.map((item) => `&sample_list=${item}`).join('')
       setSamplesId(selectedSamples)
+      setSamplesId2(selectedSamples2)
     },
     selectedRowKeys,
   }
@@ -32,6 +39,39 @@ export const SamplesTable = ({ token, samples, refreshSamples, isAdmin }) => {
       })
       refreshSamples()
     })
+  }
+
+  const showModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const handleCancel = () => {
+    setIsModalOpen(false)
+  }
+
+  const handleOk = () => {
+    navigator.clipboard.writeText(copiedText)
+    setIsModalOpen(false)
+    window.open('https://34.125.84.98/grapetree/')
+    setSelectedRowKeys([])
+  }
+
+  const fetchPhyllogenyData = async () => {
+    if (selectedRowKeys.length > 2) {
+      getPhyllogeny(token, group, samplesId2).then((response) => {
+        setCopiedText(JSON.stringify(response))
+        if (response != '') {
+          showModal()
+          navigator.clipboard.writeText(JSON.stringify(response))
+        }
+      })
+    } else {
+      notification['info']({
+        message:
+          'To obtain Phyllogeny data, you must select a minimum of three samples from the list provided below.',
+        duration: 8,
+      })
+    }
   }
 
   const columns = [
@@ -120,6 +160,29 @@ export const SamplesTable = ({ token, samples, refreshSamples, isAdmin }) => {
   ].filter((column) => !column.hidden)
   return (
     <>
+      <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <p>
+          The Phyllogeny data has been successfully copied. Please click &apos;OK&apos; to go to the
+          Grapetree site, then click on the &apos;Load files&apos; button and paste the data into
+          the designated rectangle. Finally, click on the &apos;Confirm&apos; button to proceed.
+        </p>
+      </Modal>
+      <div className={styles.samplesTableButtons}>
+        <Button onClick={fetchPhyllogenyData} type="primary">
+          Fetch Phyllogeny Data
+        </Button>
+        {isAdmin && (
+          <Popconfirm
+            title="Are you sure you want to delete?"
+            disabled={!hasSelected}
+            onConfirm={confirmDelete}
+          >
+            <Button disabled={!hasSelected} type="primary">
+              Delete
+            </Button>
+          </Popconfirm>
+        )}
+      </div>
       <Table
         pagination={false}
         dataSource={samples}
@@ -127,22 +190,9 @@ export const SamplesTable = ({ token, samples, refreshSamples, isAdmin }) => {
         rowKey={'sample_id'}
         loading={!samples}
         bordered
-        rowSelection={
-          isAdmin
-            ? {
-                columnTitle: (
-                  <Popconfirm
-                    title="Are you sure you want to delete?"
-                    disabled={!hasSelected}
-                    onConfirm={confirmDelete}
-                  >
-                    <Button shape="circle" disabled={!hasSelected} icon={<DeleteTwoTone />} />
-                  </Popconfirm>
-                ),
-                ...rowSelection,
-              }
-            : undefined
-        }
+        rowSelection={{
+          ...rowSelection,
+        }}
       />
     </>
   )
