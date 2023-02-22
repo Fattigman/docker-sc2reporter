78c03db465cb86ef7ab3eb9d33232f203e8ab288
Fattigman <jacob.karlstrom@gmail.com>
2023-02-22 14:19:19 +0100

    database name is imported from config file

--- backend/db/db.py
+++ backend/db/db.py
@@ -2,7 +2,7 @@ import motor
 from bson import json_util
 import json
 import os
-
+from api.config import settings
 """
 This is meant to be imported by the other crud files.
 The idea is to have a single place where the database is defined.
@@ -10,7 +10,7 @@ Import this file in the other crud files and use the db variable.
 """
 
 client = motor.motor_tornado.MotorClient(os.getenv('MONGO_HOST'), 27017)
-db = client['sarscov2_standalone']
-        
+db = client[settings.DATABASE_NAME]
+
 def parse_json(data):
     return json.loads(json_util.dumps(data))
