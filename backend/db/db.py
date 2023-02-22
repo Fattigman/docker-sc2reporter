cda5aaa38def4dbcd77d9f0bba6baa81c05e1786
Fattigman <jacob.karlstrom@gmail.com>
2023-02-22 15:16:40 +0100

    added auth credentials to mongodb client

--- backend/db/db.py
+++ backend/db/db.py
@@ -6,10 +6,15 @@ from api.config import settings
 """
 This is meant to be imported by the other crud files.
 The idea is to have a single place where the database is defined.
-Import this file in the other crud files and use the db variable.
+Import this file in the other crud files and use the db object.
 """
 
-client = motor.motor_tornado.MotorClient(os.getenv('MONGO_HOST'), 27017)
+client = motor.motor_tornado.MotorClient(
+    host = os.getenv('MONGO_HOST'), 
+    port = os.getenv('MONGO_PORT'), 
+    username=os.getenv('MONGO_USER'),
+    password=os.getenv('MONGO_PASS')
+    )
 db = client[settings.DATABASE_NAME]
 
 def parse_json(data):
