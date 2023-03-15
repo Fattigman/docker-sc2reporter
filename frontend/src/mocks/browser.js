5298001a3f246d391a2e8598e42fb1a824702717
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2023-03-15 15:46:49 +0100

    Improve msw get endpoints

--- frontend/src/mocks/browser.js
+++ frontend/src/mocks/browser.js
@@ -2,22 +2,23 @@ import { setupWorker, rest } from 'msw'
 
 export const { REACT_APP_BACKEND_URL } = process.env
 
+const endpoints = {
+  samples: 'samples/',
+  samplesNextclade: `samples/nextclade/?nextclade=testNextclade`,
+  samplesPango: `samples/pango/?pangolin=testPangolin`,
+  variants: `variants/?variant=testVariant`,
+  dashboard: `dashboard/?testSelectionCriterion`,
+  samplesById: `samples/sc2-1001`,
+  users: 'users/',
+  getUserInfo: 'users/me/',
+  usersDelete: `users/delete/?user=testUsername`,
+  phyllogeny: `phyllogeny/?group=group&samples`,
+}
+
 const worker = setupWorker(
-  rest.get(`${REACT_APP_BACKEND_URL}/users`, (req, res, ctx) => {
+  rest.get(`${REACT_APP_BACKEND_URL}/${endpoints.phyllogeny}`, (req, res, ctx) => {
     const statusCode = 500
-    if (statusCode === 200) {
-      return res(
-        ctx.json([
-          {
-            username: 'mock',
-            email: 'mock@email.se',
-            fullname: 'Mock Mock',
-            disabled: false,
-            scope: 'admin',
-          },
-        ])
-      )
-    } else if (statusCode === 401) {
+    if (statusCode === 401) {
       return res(ctx.status(401), ctx.json({ message: 'Unauthorized' }))
     } else if (statusCode === 404) {
       return res(ctx.status(404), ctx.json({ message: 'Not found' }))
