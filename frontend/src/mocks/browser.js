55bfcb8badad6a0670bbf00b78ebc9ae124c7b61
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2023-03-15 14:34:05 +0100

    Add error responses to MSW

--- frontend/src/mocks/browser.js
+++ frontend/src/mocks/browser.js
@@ -1,18 +1,29 @@
 import { setupWorker, rest } from 'msw'
+
 export const { REACT_APP_BACKEND_URL } = process.env
+
 const worker = setupWorker(
   rest.get(`${REACT_APP_BACKEND_URL}/users`, (req, res, ctx) => {
-    return res(
-      ctx.json([
-        {
-          username: 'mock',
-          email: 'mock@email.se',
-          fullname: 'Mock Mock',
-          disabled: false,
-          scope: 'admin',
-        },
-      ])
-    )
+    const statusCode = 500
+    if (statusCode === 200) {
+      return res(
+        ctx.json([
+          {
+            username: 'mock',
+            email: 'mock@email.se',
+            fullname: 'Mock Mock',
+            disabled: false,
+            scope: 'admin',
+          },
+        ])
+      )
+    } else if (statusCode === 401) {
+      return res(ctx.status(401), ctx.json({ message: 'Unauthorized' }))
+    } else if (statusCode === 404) {
+      return res(ctx.status(404), ctx.json({ message: 'Not found' }))
+    } else {
+      return res(ctx.status(500), ctx.json({ message: 'Internal server error' }))
+    }
   })
 )
 
