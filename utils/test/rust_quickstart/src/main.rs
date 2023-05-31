486a8286ccd58682deada793ecdd87ef9cc99e19
Fattigman <jacob.karlstrom@gmail.com>
2023-05-31 13:26:58 +0200

    now calculates frequncies of nucleotide calls

--- utils/test/rust_quickstart/src/main.rs
+++ utils/test/rust_quickstart/src/main.rs
@@ -1,6 +1,6 @@
 use mongodb::{Client, bson::{Document, doc,  oid::ObjectId}};
 use serde::{Deserialize, Serialize};
-use std::env;
+use std::{cmp, env};
 use std::error::Error;
 use futures::stream::StreamExt;
 use tokio;
@@ -11,16 +11,22 @@ struct Depth {
     _id: Option<ObjectId>,
     sample_id: String,
     sample_oid: String,
-    A: i32,
-    T: i32,
-    G: i32,
-    C: i32,
-    REFSKIP: i32,
-    DEL: i32,
-    pos: i16,
-    dp: i32,
+    A: u16,
+    T: u16,
+    G: u16,
+    C: u16,
+    REFSKIP: u16,
+    DEL: u16,
+    pos: u16,
+    dp: u16,
  }
-
+struct MatrixVector {
+    sample_id: String,
+    array: Vec<f32>,
+}
+struct Matric {
+    array: Vec<MatrixVector>
+}
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let pipeline: Vec<Document> = vec![
@@ -44,11 +50,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let depth_collection : mongodb::Collection<Vec<Depth>> = database.collection("depth");
     
     let mut results = depth_collection.aggregate(pipeline, None).await?;
-
+    // Declare variables
+    let mut sample_name : String = "no_col".to_string();
+    let mut sample_name_vector : Vec<String> = Vec::new();
+    let mut col: Vec<f32> = Vec::new();
     while let Some(result) = results.next().await {
-        // Use serde to deserialize into the MovieSummary struct:
         let doc: Depth = bson::from_document(result?)?;
-        println!("* {:?}", doc);
-     }
+        if doc.sample_id != sample_name{
+            sample_name = doc.sample_id;
+            sample_name_vector.push(sample_name.clone());
+            col.clear();
+        }
+        // Calculate the max frequency of the four nucleotides
+        let max_value = cmp::max(cmp::max(doc.A, doc.T), cmp::max(doc.G, doc.C));
+    }
+    println!("col: {:?}", sample_name_vector);
    Ok(())
 }
