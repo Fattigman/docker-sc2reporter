a71d152cb9b40cb7581ddd9107bfb962ad8f0a80
Fattigman <jacob.karlstrom@gmail.com>
2023-07-05 12:07:54 +0200

    fixed warnings

--- utils/migrations/matrix_calculator/src/main.rs
+++ utils/migrations/matrix_calculator/src/main.rs
@@ -1,17 +1,19 @@
-use mongodb::{Client, bson::{Document, doc, oid::ObjectId}};
+use mongodb::{Client, bson::{doc, oid::ObjectId}};
 use serde::{Deserialize, Serialize};
 use futures::stream::TryStreamExt;
 use std::error::Error;
 use std::{cmp};
-use ndarray::{arr2} ;
+// use ndarray::{arr2} ;
 use tokio;
+
+
 
 // Define the schema for the mongodb collections:
  #[derive(Serialize, Deserialize, Debug)]
  struct Sample {
     sample_id: String,
  }
-
+ #[allow(non_snake_case)]
  #[derive(Serialize, Deserialize, Debug)]
 struct Depth {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
@@ -50,7 +52,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
    }
 
    let depth_collection = database.collection::<Depth>("depth");
-   let mut temp : [f32; 4];
+   let mut _temp : [f32; 4];
    let mut index : Vec<String> = Vec::new();
    let mut matrix: Vec<Vec<f32>> = Vec::new();
    for name in &sample_names {
@@ -60,9 +62,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
       // Calculate an array for each sample ranging from 0 to 1 based on the frequency of each nucleotide
       while let Some(depth) = depths.try_next().await? {
          let max_value = cmp::max(cmp::max(depth.A, depth.T), cmp::max(depth.G, depth.C));
-         let temp = [depth.A as f32, depth.T as f32, depth.G as f32, depth.C as f32];
+         let _temp = [depth.A as f32, depth.T as f32, depth.G as f32, depth.C as f32];
          for i in 0..4 {
-            column.push(temp[i] / max_value as f32);
+            column.push(_temp[i] / max_value as f32);
          }
          // Appends index vector with each position and nucleotide
          for char in ['A', 'T', 'G', 'C'].iter() {
