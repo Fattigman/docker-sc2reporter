56e5d5096d1d43c21230fa9d4c16d99c763036eb
Fattigman <jacob.karlstrom@gmail.com>
2023-09-18 10:13:46 +0200

    uncommented tests

--- backend/tests/test_root.py
+++ backend/tests/test_root.py
@@ -1,31 +1,21 @@
-# import json
+import json
 
-# from .test_base import client
-
-
-# def test_root():
-#     response = client.get("/")
-#     print('Testing "/" endpoint...')
-#     assert response.status_code == 200
-#     print("OK!")
+from .test_base import client
 
 
-# def test_login():
-#     payload = "grant_type=&username=Test&password=Test&client_id=&client_secret="
-#     response = client.post(
-#         "/login/token",
-#         headers={
-#             "Content-Type": "application/x-www-form-urlencoded",
-#             "accept": "application/json",
-#         },
-#         data=payload,
-#     )
-#     assert response.status_code == 200
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
 
 
-# def test_read_main():
-#     test_root()
-#     test_login()
-
-
-# test_read_main()
+def test_login():
+    payload = "grant_type=&username=Test&password=Test&client_id=&client_secret="
+    response = client.post(
+        "/login/token/",
+        headers={
+            "Content-Type": "application/x-www-form-urlencoded",
+            "accept": "application/json",
+        },
+        data=payload,
+    )
+    assert response.status_code == 200
